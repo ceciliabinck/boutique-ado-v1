699de0069bb527dc0e746e23cf1a29e15b3f8803
ceciliabinck <cecilia4binck@gmail.com>
2020-10-04 12:53:01 +0000

    Add checkout functionality

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -7,9 +7,9 @@
 */
 
 
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripe_public_key);
+var stripePublickey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublickey);
 var elements = stripe.elements();
 
 var style = {
@@ -30,3 +30,18 @@ var style = {
 
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
+
+// Handle realtime validation errors on the card element
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = 
+            `<span class="icon" role="alert">
+            <i class="fas fa-times"></i></span>
+            <span>${event.error.message}</span>`
+        ;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
